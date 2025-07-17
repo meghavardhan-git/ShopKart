58a03d61b6998e3de8b3795c9337f15cc15e47ff
meghavardhan-git <meghanani122005@gmail.com>
2025-07-17 18:43:28 +0530

    Clean up media folder, allow media/uploads tracking

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA FILES
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
